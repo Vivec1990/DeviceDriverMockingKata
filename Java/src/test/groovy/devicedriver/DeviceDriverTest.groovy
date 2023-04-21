cdebda6f31091c5d0da8652bee2a7b937edbaa20
Vivec1990 <meyer_sebastian@outlook.com>
2023-04-21 15:02:43 +0200

    cover all of DeviceDriver.java except the timeout exception

--- Java/src/test/groovy/devicedriver/DeviceDriverTest.groovy
+++ Java/src/test/groovy/devicedriver/DeviceDriverTest.groovy
@@ -1,8 +1,15 @@
 package devicedriver
 
+
 import spock.lang.Specification
 
 class DeviceDriverTest extends Specification {
+
+    def INIT_ADDRESS = 0x00
+    def PROGRAM_COMMAND = 0x40
+
+    def address = 1000
+    def data = 0x22
 
     def "read from hardware"() {
         given:
@@ -13,7 +20,84 @@ class DeviceDriverTest extends Specification {
         def data = driver.read(0xFF)
 
         then:
-        data == (byte)0
+        1 * hardware.read(0xFF) >> 50
+        data == (byte) 50
+    }
+
+    def "write to vpp protected range"() {
+        given:
+        FlashMemoryDevice hardware = Mock()
+        def driver = new DeviceDriver(hardware)
+
+        when:
+        driver.write(address, (byte) data)
+
+        then:
+        1 * hardware.write(INIT_ADDRESS, PROGRAM_COMMAND)
+        1 * hardware.write(address, data)
+        1 * hardware.read(INIT_ADDRESS) >> 0x20
+        thrown VppException
+    }
+
+    def "write encounters an internal error"() {
+        given:
+        FlashMemoryDevice hardware = Mock()
+        def driver = new DeviceDriver(hardware)
+
+        when:
+        driver.write(address, (byte) data)
+
+        then:
+        1 * hardware.write(INIT_ADDRESS, PROGRAM_COMMAND)
+        1 * hardware.write(address, data)
+        1 * hardware.read(INIT_ADDRESS) >> 0x10
+        thrown InternalErrorException
+    }
+
+    def "write to protected block"() {
+        given:
+        FlashMemoryDevice hardware = Mock()
+        def driver = new DeviceDriver(hardware)
+
+        when:
+        driver.write(address,(byte) data)
+
+        then:
+        1 * hardware.write(INIT_ADDRESS, PROGRAM_COMMAND)
+        1 * hardware.write(address, data)
+        1 * hardware.read(INIT_ADDRESS) >> 0x08
+        thrown ProtectedBlockException
+    }
+
+    def "write encounters read failure"() {
+        given:
+        FlashMemoryDevice hardware = Mock()
+        def driver = new DeviceDriver(hardware)
+
+        when:
+        driver.write(address,(byte) data)
+
+        then:
+        1 * hardware.write(INIT_ADDRESS, PROGRAM_COMMAND)
+        1 * hardware.write(address, data)
+        1 * hardware.read(0x00) >> 0x02
+        1 * hardware.read(address) >> _
+        thrown ReadFailureException
+    }
+
+    def "write is successful"() {
+        given:
+        FlashMemoryDevice hardware = Mock()
+        def driver = new DeviceDriver(hardware)
+
+        when:
+        driver.write(address,(byte) data)
+
+        then:
+        1 * hardware.write(INIT_ADDRESS, PROGRAM_COMMAND)
+        1 * hardware.write(address, data)
+        1 * hardware.read(INIT_ADDRESS) >> 0x02
+        1 * hardware.read(address) >> data
     }
 
 }
